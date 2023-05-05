6de88cb888a3367213806dcf50e6494352fb0b4a
Hansel Bormey <hanselbormey@gmail.com>
2023-05-05 11:04:05 -0400

    Add constants

---
+++ utils/constants.ts
@@ -0,0 +1,20 @@
+export const COLOR = {
+  mention: '#22c55e',
+  hashtag: '#6366f1',
+  email: '#ca8a04',
+  link: '#3b82f6',
+};
+
+export const TXT_COLOR = {
+  mention: 'text-green-500',
+  hashtag: 'text-indigo-500',
+  email: 'text-orange-500',
+  link: 'text-blue-500',
+};
+
+export const BG_COLOR = {
+  mention: 'bg-green-200',
+  hashtag: 'bg-indigo-200',
+  email: 'bg-orange-200',
+  link: 'bg-blue-200',
+};
