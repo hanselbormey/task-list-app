1da8846dd4e395b8f7cb23af7b51a9dfa94c747c
Hansel Bormey <hanselbormey@gmail.com>
2023-05-05 15:10:13 -0400

    Remove margin

--- components/List.tsx
+++ components/List.tsx
@@ -3,7 +3,7 @@ import ListItem from './ListItem';
 
 export default function List({ data }: { data: Task[] }) {
   return (
-    <div className="mt-4 p-1">
+    <div className="p-1">
       {data?.map((item) => (
         <ListItem key={item.id} item={item} />
       ))}
