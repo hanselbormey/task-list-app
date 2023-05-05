c242cb73166baf990ad1e2aa98a2c3695f4332f2
Hansel Bormey <hanselbormey@gmail.com>
2023-05-05 11:04:49 -0400

    Add mock data

---
+++ utils/mockData.ts
@@ -0,0 +1,8 @@
+export const DataList = [
+  {
+    id: '1',
+    body: 'I should do the landing page and send email to cubaneng@gmail.com #UI/UX',
+  },
+  { id: '2', body: '@dev is doing the auth login #backend' },
+  { id: '3', body: 'Check this  www.myurl.com and send feedback to @user' },
+];
