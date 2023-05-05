b25966c29eb7b040f4b2ddbd834340ba1de66d43
Hansel Bormey <hanselbormey@gmail.com>
2023-05-05 12:38:18 -0400

    Adjust colors

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,20 +1,20 @@
 export const COLOR = {
-  mention: '#22c55e',
-  hashtag: '#6366f1',
-  email: '#ca8a04',
+  mention: '#4ade80',
+  hashtag: '#a855f7',
+  email: '#fb923c',
   link: '#3b82f6',
 };
 
 export const TXT_COLOR = {
-  mention: 'text-green-500',
-  hashtag: 'text-indigo-500',
-  email: 'text-orange-500',
+  mention: 'text-green-400',
+  hashtag: 'text-purple-500',
+  email: 'text-orange-400',
   link: 'text-blue-500',
 };
 
 export const BG_COLOR = {
-  mention: 'bg-green-200',
-  hashtag: 'bg-indigo-200',
-  email: 'bg-orange-200',
-  link: 'bg-blue-200',
+  mention: 'bg-green-100',
+  hashtag: 'bg-purple-100',
+  email: 'bg-orange-100',
+  link: 'bg-blue-100',
 };
