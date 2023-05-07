0e88f617d051f21613aed8655c299dcec87d9b5f
Hansel Bormey <hanselbormey@gmail.com>
2023-05-06 23:24:25 -0400

    Update mock data

--- utils/mockData.ts
+++ utils/mockData.ts
@@ -1,8 +1,8 @@
 export const DataList = [
   {
     id: '1',
-    body: '#important Write to some_email@gmail.com and tell @hansel about https://staging.alldone.app',
+    body: '#important Write to some_email@gmail.com and tell @natasha about https://staging.alldone.app',
   },
-  { id: '2', body: '@dev is doing the auth login #backend' },
-  { id: '3', body: 'Check this  www.myurl.com and send feedback to @user' },
+  { id: '2', body: '@natasha will do the auth login #backend' },
+  { id: '3', body: 'Check this https://www.myurl.com #UI/UX' },
 ];
