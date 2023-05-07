97e287d0eb9f2e77e5a25a2cf5ee0b4aa9490e1f
Hansel Bormey <hanselbormey@gmail.com>
2023-05-06 23:30:55 -0400

    Update List component

--- components/List.tsx
+++ components/List.tsx
@@ -3,7 +3,7 @@ import ListItem from './ListItem';
 
 export default function List({ data }: { data: Task[] }) {
   return (
-    <div className="p-1">
+    <div className="p-0.5">
       {data?.map((item) => (
         <ListItem key={item.id} item={item} />
       ))}
