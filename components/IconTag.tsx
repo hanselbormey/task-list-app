5548e402dea66a1797b2de62fb5011d0e52344fd
Hansel Bormey <hanselbormey@gmail.com>
2023-05-06 23:27:13 -0400

    Change styles

--- components/IconTag.tsx
+++ components/IconTag.tsx
@@ -1,41 +1,43 @@
 import { Hash, Link, Mail } from 'react-feather';
 
 import { Tag } from '@/types/enums';
-import { IMG_SRC } from '@/utils/constants';
-import Avatar from './Avatar';
 
 const IconTag: React.FC<{ tag: Tag; label: string }> = ({ tag, label }) => {
   switch (tag) {
     case Tag.EMAIL:
       return (
         <a className="flex items-center" href={`mailto:${label}`}>
-          <Mail className="w-4 h-4 mr-1" />
-          Mail
+          <Mail className="w-4 h-4" />
+          <p className="mx-1">Mail</p>
         </a>
       );
     case Tag.LINK:
       return (
         <a
-          className="flex items-center"
+          className="flex items-center hover:cursor-pointer"
           rel="external"
           href={!label.includes('http') ? `https://${label}` : label}
+          target="_blank"
         >
-          <Link className="w-4 h-4 mr-1" />
-          Link
+          <Link className="w-4 h-4" />
+          <p className="mx-1">Link</p>
         </a>
       );
     case Tag.HASHTAG:
       return (
         <a className="flex items-center">
-          <Hash className="w-4 h-4 mr-1" />
-          {label.slice(1)}
+          <Hash className="w-4 h-4" />
+          <p className="mx-1">{label.slice(1)}</p>
         </a>
       );
     case Tag.MENTION:
       return (
         <a className="flex items-center">
-          <Avatar classname="mr-1" src={IMG_SRC} />
-          {label.slice(1)}
+          <img
+            className="object-cover w-5 h-5 rounded-xl"
+            src="/woman_avatar.jpg"
+          />
+          <p className="mx-1">{label.slice(1)}</p>
         </a>
       );
   }
