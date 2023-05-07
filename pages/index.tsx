d0d0124cb969a76db14ad3145ff673ec63d6aeca
Hansel Bormey <hanselbormey@gmail.com>
2023-05-06 23:32:13 -0400

    Add handleSubmit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
-import Input from '@/components/Input';
 import { useEffect, useState } from 'react';
 
+import Input from '@/components/Input';
 import List from '@/components/List';
 import Disclosure from '@/components/Disclosure';
 import { DataList } from '@/utils/mockData';
+import { Task } from '@/types/task';
 
 export default function Home() {
   const [value, setValue] = useState<string>('');
   const [initialValue, setInitialValue] = useState<string>('');
   const [action, setAction] = useState<string>('');
   const [disabledButtons, setDisabledButtons] = useState<boolean>(true);
+
+  const [dataList, setDataList] = useState<Task[]>(DataList);
 
   useEffect(() => {
     value ? setDisabledButtons(false) : setDisabledButtons(true);
@@ -28,20 +31,36 @@ export default function Home() {
     return result;
   };
 
+  const addItem = (value: string) => {
+    const arr: Task[] = [...dataList];
+    arr.unshift({ id: Math.random().toString(), body: value });
+    setValue('');
+    setDataList(arr);
+  };
+
+  const handleSubmit = (act: string) => {
+    if (act === 'add') {
+      addItem(value);
+    }
+    if (act === 'ok') {
+      addItem('New task to do');
+    }
+  };
+
   return (
     <div className="mt-4">
-      <h1 className="text-3xl text-blue-500">Task List App</h1>
+      <h1 className="text-3xl font-semibold text-blue-500">Task List App</h1>
       <main className="mt-16">
         <Disclosure
           formAction={action}
           disabledButtons={disabledButtons}
-          onSubmit={(e: string) => alert(e)}
+          onSubmit={handleSubmit}
           onCancel={() => setValue('')}
         >
           <Input value={value} onChange={(val) => setValue(val)} />
         </Disclosure>
         <div className="ml-4">
-          <List data={DataList} />
+          <List data={dataList} />
         </div>
       </main>
     </div>
