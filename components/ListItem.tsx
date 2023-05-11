497d01de1dafe1bbca1ff99f35627c937502ac24
Hansel Bormey <hanselbormey@gmail.com>
2023-05-11 19:05:38 -0400

    Update ListItem
    -Create styledText
    -Render input when the listItem is clicked

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,15 +1,16 @@
+import { useState } from 'react';
 import cn from 'classnames';
 
+import Input from './Input';
 import { Task } from '@/types/task';
 import Chip from './Chip';
 import IconTag from './IconTag';
 import { getBgColor, getTag, getTextColor } from '../utils';
 
-const ListItem = ({ item }: { item: Task }) => {
+const StyledText = ({ text }: { text: string }) => {
   return (
-    <div className="flex items-center flex-wrap p-0.5">
-      <input type="checkbox" className="rounded-sm mr-2" />
-      {item.body.split(' ').map((e, index) => {
+    <>
+      {text.split(' ').map((e, index) => {
         const tag = getTag(e);
         if (tag) {
           const textColor = getTextColor(tag);
@@ -22,6 +23,29 @@ const ListItem = ({ item }: { item: Task }) => {
         }
         return <span key={index}>{e}&nbsp;</span>;
       })}
+    </>
+  );
+};
+
+const ListItem = ({ item }: { item: Task }) => {
+  const [value, setValue] = useState<string>('');
+
+  const [open, setOpen] = useState<boolean>(false);
+
+  return (
+    <div
+      className={cn(
+        'flex items-center',
+        `${!open ? 'flex-wrap' : 'flex-nowrap'}`
+      )}
+      onClick={() => setOpen(!open)}
+    >
+      <input type="checkbox" className="rounded-sm mr-2" />
+      {!open ? (
+        <StyledText text={item.body} />
+      ) : (
+        <Input id={item.id} value={item.body} onChange={(e) => setValue(e)} />
+      )}
     </div>
   );
 };
